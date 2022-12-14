2c705774fa1b443c48d2d00e773385cdfdca8ead
PekayTab <pekaytab@gmail.com>
2022-12-14 01:48:13 -0500

    refactor: :sparkles: Codebase reformat for ease of use

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import { Laptop, Logo, Title, SubTitle } from '@/components/pages/index'
+import styles from '@/styles/index.module.scss'
+
 const Index = () => (
 	<>
 		<section className='main_content index'>
 			<span className='colored_circle' />
-			<div className='row'>
-				<div className='content'>
-					<Logo />
-					<Title />
-					<SubTitle />
+			<div className={styles.row}>
+				<div className={styles.content}>
+					<div className={styles.textWrapper}>
+						<Logo />
+						<Title />
+						<SubTitle />
+					</div>
 					<Laptop />
 				</div>
 			</div>
